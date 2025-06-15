a50b4889de8fcc3b7e846bd0e6ce610034f691e5
IT Semy <semy@fukuryo.co.id>
2025-06-15 16:19:21 +0700

    feat: register

---
+++ src/components/views/Register/useRegister.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { IRegister } from "@/types/Auth";
+import authServices from "@/services/auth";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
+const registerSchema = yup.object().shape({
+  fullName: yup.string().required("please input fullname"),
+  username: yup.string().required("please input username"),
+  email: yup
+    .string()
+    .email("Email format not valid")
+    .required("please input your email"),
+  password: yup
+    .string()
+    .min(8, "Minimal 8 Character")
+    .required("Please input your password"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), ""], "Password not match")
+    .required("Please input your confirmation password"),
+});
+
+const useRegister = () => {
+  const router = useRouter();
+  const [visiblePassword, setVisiblePassword] = useState({
+    password: false,
+    confirmPassword: false,
+  });
+
+  const handleVisiblePassword = (key: "password" | "confirmPassword") =>
+    setVisiblePassword({ ...visiblePassword, [key]: !visiblePassword[key] });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setError,
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const registerService = async (payload: IRegister) => {
+    const results = await authServices.register(payload);
+
+    return results;
+  };
+
+  const { mutate: mutateRegister, isPending: isPendingRegister } = useMutation({
+    mutationFn: registerService,
+    onError(error) {
+      setError("root", {
+        message: error.message,
+      });
+    },
+    onSuccess: () => {
+      router.push("/auth/register/success");
+      reset();
+    },
+  });
+
+  const handleRegister = (data: IRegister) => mutateRegister(data);
+
+  return {
+    visiblePassword,
+    handleVisiblePassword,
+    control,
+    handleSubmit,
+    handleRegister,
+    isPendingRegister,
+    errors,
+  };
+};
+
+export default useRegister;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import "@/styles/globals.css";
+import { cn } from "@/utils/cn";
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <main
+          className={cn(
+            inter.className,
+            "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py-0",
+          )}
+        >
+          <Component {...pageProps} />
+        </main>
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
